73ac85d7b8a1560f60847d3a9c981b7da7f0dd68
nebkat <nebkat@teamhacksung.org>
2012-05-16 16:54:44 +0200

    Add possibility to filter rss by device

--- getcm/handlers.py
+++ getcm/handlers.py
@@ -115,6 +115,7 @@ class Base62Handler(BaseHandler):
 
 class RssHandler(BaseHandler):
     def get(self):
-        files = File.browse(None, None, 100)
+        device = self.request.arguments.get('device', [None])[0]
+        files = File.browse(device, None, 100)
         self.set_header('Content-Type', "application/xml; charset=utf-8")
         self.render("rss.mako", {'files': files})
