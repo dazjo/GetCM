6be721b96a5920d06f9b14fd654cc6d8ce91b680
Chris Soyars <ctso@ctso.me>
2012-06-15 23:07:26 -0400

    Use tornado 2.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="GetCM",
     version=version,
     packages=find_packages(),
-    install_requires=['tornado', 'sqlalchemy', 'mako', 'python-android'],
+    install_requires=['tornado==2.2', 'sqlalchemy', 'mako', 'python-android'],
     entry_points={
         'console_scripts': [
             'getcm.server=getcm.app:run_server',
