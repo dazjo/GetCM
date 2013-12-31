9175e2283370d11919e0fbb83ec35d9483fef078
Daz Jones <yuki@thebronasium.com>
2013-12-31 15:50:20 +0100

    devicemap: add Huawei Ascend G330 (u8825)

--- getcm/model/devicemap.py
+++ getcm/model/devicemap.py
@@ -2,6 +2,7 @@ devicemap = {
     'blade2_p736e': 'ZTE Crescent (TMO)',
     'blade2_p736v': 'ZTE Crescent (OUK)',
     'u8815': 'Huawei Ascend G300',
+    'u8825': 'Huawei Ascend G330',
     'u8833': 'Huawei Ascend Y300',
     'u8951': 'Huawei Ascend G510'
 }
